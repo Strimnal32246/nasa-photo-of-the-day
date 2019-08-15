8491c2f253a168354348193372e9056587b9adf8
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-08-15 08:31:57 -0400

    finished added the picture and code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
+function App(props) {
+  const [pic, setPic] = useState();
+  const [date, setDate] = useState();
+  const [title, setTitle] = useState();
+  const [expla, setExpla] = useState();
+
+  //axios function
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=RlVu0xWKQ2LGh5zKsgeSnpciQJvSxmA4e0OxOBqc"
+      )
+      .then(response => {
+        //console.log(response)
+        const imageNasa = response.data.hdurl;
+        console.log("Nasa image", imageNasa);
+        setPic(imageNasa);
+
+        const dateNasa = response.data.date;
+        console.log("Nasa date: ", dateNasa);
+        setDate(dateNasa);
+
+        const titleNasa = response.data.title;
+        console.log("Nasa title: ", titleNasa);
+        setTitle(titleNasa);
+
+        const explanationNasa = response.data.explanation;
+        console.log("Nasa explanation: ", explanationNasa);
+        setExpla(explanationNasa);
+      });
+  }, []);
   return (
     <div className="App">
+      <div className="photoOfDay">
+        <img style={{ width: 500, height: 500 }} src={pic} />
+      </div>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        {title} || {date}
       </p>
+      <p>{expla}</p>
     </div>
   );
 }
 
+//exporting data-
 export default App;
